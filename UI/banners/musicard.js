2db16e8d042fca3bb640c42923bd2a1f9ac6a359
eriseris <69521825+eriseris@users.noreply.github.com>
2024-10-16 18:54:11 +0400

    fix banner

--- UI/banners/musicard.js
+++ UI/banners/musicard.js
@@ -3,8 +3,8 @@ const path = require('path');
 const data = {
     backgroundImages: [
         path.resolve(__dirname, '../musicimages/sam.png'),
-        path.resolve(__dirname, '../musicimages/sam1.png'),
         path.resolve(__dirname, '../musicimages/sam2.png'),
+        path.resolve(__dirname, '../musicimages/sam3.png'),
     ],
 };
 
